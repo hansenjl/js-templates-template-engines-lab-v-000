ea3de1f8366a2a9321f186918230b2b798f1c439
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2018-08-08 15:30:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 function init(){
-   var pageTemplate = document.getElementById("page-template").innerHTML;
- 
-  //create template function
+  //page template
+  var pageTemplate = document.getElementById("page-template").innerHTML;
   var templateFn = _.template(pageTemplate);
+  
+  //post template
+  var postTemplate = document.getElementById("post-template").innerHTML;
+  var postTemplateFn = _.template(postTemplate);
+  
+  var 
 
   var main = document.getElementsByTagName('main')[0];
  
   //append rather than replace!
-  main.innerHTML = templateFn();
+  main.innerHTML += templateFn();
 }
